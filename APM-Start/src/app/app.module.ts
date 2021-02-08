6e987a82b96e6fc7d6900eb848205b2c10f6a976
JamalGuyo <jamalguyo08@gmail.com>
2021-02-08 11:48:40 +0300

    move routing to separate module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
+import {AppRoutingModule} from './app-routing.module'
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
@@ -21,24 +20,10 @@ import { MessageModule } from './messages/message.module';
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    RouterModule.forRoot([
-      {
-        path: 'welcome',
-        component: WelcomeComponent,
-      },
-      {
-        path: '',
-        redirectTo: 'welcome',
-        pathMatch: 'full',
-      },
-      {
-        path:'**',
-        component: PageNotFoundComponent
-      }
-    ]),
     ProductModule,
     UserModule,
     MessageModule,
+    AppRoutingModule
   ],
   declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
   bootstrap: [AppComponent],
