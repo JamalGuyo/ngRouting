bc03e6089e5397cc2039ad9b8a58c21481ebf662
JamalGuyo <jamalguyo08@gmail.com>
2021-02-17 08:52:46 +0300

    create slideinanimation on router-outlet

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Router } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 
+import { slideInAnimation } from './app.animation';
+
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation],
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
